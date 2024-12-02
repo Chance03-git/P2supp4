831400ee56bd4b54e6eba17062c36e9dc31dc9c5
Chance03-git <Chancefoster05@gmail.com>
2024-12-01 20:19:27 -0500

    Test function for generate formatted Hex Strings

---
+++ hexadecimal.java
@@ -0,0 +1,7 @@
+public class Hexadecimal {
+        public static void main(String[] args) {
+        // Test the method
+        String hexString = generateFormattedHexString();
+        System.out.println("Generated Hexadecimal String: " + hexString);
+    }
+}
