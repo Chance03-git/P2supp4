1b302c0f08c7c6850ac310b0229f2ed752c874e7
Chance03-git <Chancefoster05@gmail.com>
2024-12-01 20:52:04 -0500

    Correction and test function for StringArrayHandler

--- arrayfields.java
+++ arrayfields.java
@@ -1,10 +1,17 @@
 public class arrayfields {
     public static void main(String[] args) {
-        // Test the hashString method
-        String input = "Hello, World!";
-        String hashedOutput = hashString(input);
-        System.out.println("Input String: " + input);
-        System.out.println("Hashed String: " + hashedOutput);
+        // Test the StringArrayHandler class
+        StringArrayHandler handler = new StringArrayHandler(5);
+
+        // Insert strings into the array
+        handler.insert("Hello", 0);
+        handler.insert("World", 1);
+        handler.insert("Overflow", 7); // This will wrap around to index 2
+        handler.insert("Java", 4);
+        handler.insert("Example", 5); // This will wrap around to index 0
+
+        // Display the array contents
+        System.out.println("Array Contents: " + handler.getArrayContents());
     }
 }
 
