3d42ff3667a023696290b3f28471acc70321e8cc
Chance03-git <Chancefoster05@gmail.com>
2024-12-01 20:47:41 -0500

    test for hash string method

---
+++ arrayfields.java
@@ -0,0 +1,10 @@
+public class arrayfields {
+    public static void main(String[] args) {
+        // Test the hashString method
+        String input = "Hello, World!";
+        String hashedOutput = hashString(input);
+        System.out.println("Input String: " + input);
+        System.out.println("Hashed String: " + hashedOutput);
+    }
+}
+
